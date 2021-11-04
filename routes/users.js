64b3a8fabf356b4253c98b1cd218043810287693
Elie Marouny <EMarouny@areeba.com>
2021-11-04 15:09:24 +0200

    passport jwt

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ var express = require('express');
 const bodyParser = require('body-parser');
 var User = require('../models/user');
 var passport = require('passport');
+var authenticate = require('../authenticate');
 
 var router = express.Router();
 router.use(bodyParser.json());
@@ -30,10 +31,13 @@ router.post('/signup', (req, res, next) => {
 });
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
+
+    var token = authenticate.getToken({ _id: req.user._id });
     res.statusCode = 200;
     res.setHeader('Content-Type', 'application/json');
-    res.json({ success: true, status: 'Login successful!' });
+    res.json({ success: true, token: token, status: 'Login successful!' });
 });
+
 router.get('/logout', (req, res) => {
     if (req.session) {
         req.session.destroy();
@@ -46,4 +50,5 @@ router.get('/logout', (req, res) => {
         next(err);
     }
 });
+
 module.exports = router;
